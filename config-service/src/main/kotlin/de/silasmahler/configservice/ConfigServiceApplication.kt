b0a3c077b682ede6b29e015f3a207ae0db109cb3
SilasMahler <simah002@stud.fh-dortmund.de>
2017-12-02 19:48:26 +0100

    updated a bunch of configuration

--- config-service/src/main/kotlin/de/silasmahler/configservice/ConfigServiceApplication.kt
+++ config-service/src/main/kotlin/de/silasmahler/configservice/ConfigServiceApplication.kt
@@ -1,11 +1,13 @@
 package de.silasmahler.configservice
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableAutoConfiguration
 class ConfigServiceApplication
 
 fun main(args: Array<String>) {
