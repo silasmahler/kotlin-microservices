4b3169aca72d3fd69445516f78ed65bd78f22a41
SilasMahler <simah002@stud.fh-dortmund.de>
2017-12-11 16:35:12 +0100

    updated config

--- hystrix-service/src/main/kotlin/de/silasmahler/hystrixservice/HystrixServiceApplication.kt
+++ hystrix-service/src/main/kotlin/de/silasmahler/hystrixservice/HystrixServiceApplication.kt
@@ -1,9 +1,15 @@
 package de.silasmahler.hystrixservice
 
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigurationProperties
+@EnableAutoConfiguration
 class HystrixServiceApplication
 
 fun main(args: Array<String>) {
