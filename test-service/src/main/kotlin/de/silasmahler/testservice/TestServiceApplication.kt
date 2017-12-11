248c811fdca950051e96aeafb25574beada0d181
SilasMahler <simah002@stud.fh-dortmund.de>
2017-12-11 17:06:00 +0100

    updated config

--- test-service/src/main/kotlin/de/silasmahler/testservice/TestServiceApplication.kt
+++ test-service/src/main/kotlin/de/silasmahler/testservice/TestServiceApplication.kt
@@ -2,10 +2,10 @@ package de.silasmahler.testservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 class TestServiceApplication
 
 fun main(args: Array<String>) {
