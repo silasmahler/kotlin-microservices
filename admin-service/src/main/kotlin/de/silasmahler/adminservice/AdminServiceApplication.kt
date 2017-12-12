46871861125cfdcb1681849f2d2db79e522906ab
SilasMahler <simah002@stud.fh-dortmund.de>
2017-12-12 15:18:18 +0100

    spring boot admin wip

--- admin-service/src/main/kotlin/de/silasmahler/adminservice/AdminServiceApplication.kt
+++ admin-service/src/main/kotlin/de/silasmahler/adminservice/AdminServiceApplication.kt
@@ -1,12 +1,16 @@
 package de.silasmahler.adminservice
 
 import de.codecentric.boot.admin.config.EnableAdminServer
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableAutoConfiguration
+@Configuration
 @EnableAdminServer
 class AdminServiceApplication
 
