1e3342c93a0d03384990bd7f584837cb3fbce864
SilasMahler <simah002@stud.fh-dortmund.de>
2017-12-12 12:44:34 +0100

    updated missing dependencies for zuul

--- test-service/src/main/kotlin/de/silasmahler/testservice/TestServiceApplication.kt
+++ test-service/src/main/kotlin/de/silasmahler/testservice/TestServiceApplication.kt
@@ -3,6 +3,10 @@ package de.silasmahler.testservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,4 +14,17 @@ class TestServiceApplication
 
 fun main(args: Array<String>) {
     runApplication<TestServiceApplication>(*args)
+}
+
+data class Greeting(val id: Long, val content: String)
+
+@RestController
+class GreetingsController {
+    val counter = AtomicLong()
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value= "name", defaultValue = "World")
+                 name: String)
+    = Greeting(counter.incrementAndGet(), "Hello, $name")
+
 }
